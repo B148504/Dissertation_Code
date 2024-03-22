a6d30498149da0514c97056b9738c3b185c04025
swift-213 <frances.swift@outlook.com>
2024-03-22 15:19:50 +0000

    chroms flag now optional

--- Repeat_finder.py
+++ Repeat_finder.py
@@ -2,25 +2,6 @@ import argparse
 from argparse import ArgumentParser
 import pandas as pd
 import numpy as np
-parser=ArgumentParser()
-parser.add_argument("-i", "--input", help="Add in the VCF file under this flag", required=True)
-parser.add_argument("-o", "--output", action='store', help="Add in a text file to get the dictionary output and the positions of the indels", required=False)
-parser.add_argument("-i2", "--input2", help="Add in the VCF file under this flag", required=False)
-args=parser.parse_args()
-
-fasta_file=args.input
-chrom_file=args.input2
-output=args.output
-
-file1=open(chrom_file, 'r')
-
-Lines=file1.readlines()
-
-chrom=[]
-for line in Lines:
-    chrom.append("{}".format(line.strip()))
-
-
 
 def parseFasta(string, makeUppercase=False):
     splitString = string.split(">")[1:]
@@ -29,19 +10,44 @@ def parseFasta(string, makeUppercase=False):
     if makeUppercase: seqs = [s.upper() for s in seqs]
     return (names,seqs)
 
+parser=ArgumentParser()
+parser.add_argument("-fasta", "--fasta_input", help="Add in the fasta file under this flag", required=True)
+parser.add_argument("-soft_masked_regions_bed_output", "--output", action='store', help="Bed output of soft masked regions", required=False)
+parser.add_argument("-chroms", "--chromosome_file", help="contig/scaffold/chromosomes that you want to include in this analysis", required=False)
+args=parser.parse_args()
+
+fasta_file=args.fasta_input
+chrom_file=args.chromosome_file
+output=args.output
 
 with open(fasta_file) as f:
     allText = f.read()
- 
 
 names, seqs = parseFasta(allText)
 
-
 names_starts_and_ends = []
-
-
-for i in range(len(names)):
-    if names[i] in chrom:
+if chrom_file:
+    file1=open(chrom_file, 'r')
+    Lines=file1.readlines()
+    chrom=[]
+    for line in Lines:
+        chrom.append("{}".format(line.strip()))
+    for i in range(len(names)):
+        if names[i] in chrom:
+            print(names[i])
+            inside = False
+            for j in range(len(seqs[i])):
+                if inside == False:
+                    if seqs[i][j].islower():
+                        inside= True
+                        current = [names[i], j]
+                else:
+                    if not seqs[i][j].islower():
+                        current.append(j)
+                        names_starts_and_ends.append(current)
+                        inside = False
+else:
+    for i in range(len(names)):
         print(names[i])
         inside = False
         for j in range(len(seqs[i])):
@@ -54,12 +60,8 @@ for i in range(len(names)):
                     current.append(j)
                     names_starts_and_ends.append(current)
                     inside = False
-         
 
 
+names_starts_and_ends_df = pd.DataFrame(names_starts_and_ends, columns=['Chrom', "Start", 'End'])
 
-with open(output, 'w') as f:
-    for item in names_starts_and_ends:
-        f.write(str(item) + "\n")
-    f.close()
-   
+names_starts_and_ends_df.to_csv(output, sep ='\t' , index = False, header = False)
